b5854dd7bb520dc99d1e328624dad899112e54d9
Alan <king.python@mail.ru>
2025-05-08 13:57:02 +0300

    еще изменения

--- api/endpoints/worlds.py
+++ api/endpoints/worlds.py
@@ -6,7 +6,7 @@ from db.models import World, Word, Sentence
 from core.security import get_current_user
 from db.models import User
 from typing import List
-from core.file_storage import upload_image
+from core.file_storage import upload_image, upload_base64
 from pydantic import BaseModel
 import json
 from sqlalchemy import and_
@@ -29,6 +29,25 @@ async def get_all_worlds(db: Session = Depends(get_db)):
         ) 
         for world in public_worlds
     ]
+
+
+@router.get("/userWorlds", response_model=List[WorldPreview])
+async def get_user_worlds(
+        db: Session = Depends(get_db),
+        current_user: User = Depends(get_current_user)
+):
+    """Получить все мирки текущего пользователя"""
+    user_worlds = db.query(World).filter(World.author_id == current_user.id).all()
+
+    return [
+        WorldPreview(
+            id=world.id,
+            title=world.title,
+            image=world.image
+        )
+        for world in user_worlds
+    ]
+
 
 @router.get("/{world_id}", response_model=WorldDetail)
 async def get_world(world_id: int, db: Session = Depends(get_db)):
@@ -79,13 +98,16 @@ async def create_world(
     db: Session = Depends(get_db),
     current_user: User = Depends(get_current_user)
 ):
+
+
+    image_url = await upload_base64(world_data.image)
     # Создаем мир
     db_world = World(
         title=world_data.title,
         description=world_data.description,
         is_public=world_data.is_public,
         author_id=current_user.id,
-        image=world_data.image
+        image=image_url
     )
     db.add(db_world)
     db.commit()
